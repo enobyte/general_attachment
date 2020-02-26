924e177269624a787a4bf535f041c371c316419d
enobyte <enoraden@gmail.com>
2020-02-26 15:38:47 +0700

    fixing url

--- controllers/upload_profile.go
+++ controllers/upload_profile.go
@@ -89,7 +89,7 @@ var UploadPhotoProfile = func(w http.ResponseWriter, r *http.Request) {
 		resp := utils.Message(utils.BadReq(), false, "uploaded failed", nil)
 		utils.Respond(w, resp)
 	} else {
-		imageUrl := "http://103.107.103.56:8080/claimrembursement/" + filepath.Base(tempFile.Name())
+		imageUrl := "http://103.107.103.56:8080/profile/" + filepath.Base(tempFile.Name())
 		resp := utils.Message(utils.SuccesReq(), true, "data uploaded", map[string]interface{}{"url": imageUrl})
 		utils.Respond(w, resp)
 	}
